31f4cde9c6e8a97af33c0675fa548365b6fe4620
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-01-20 17:45:25 +0100

    Sanity Checks

--- src/Saab95.java
+++ src/Saab95.java
@@ -35,12 +35,16 @@ public class Saab95 extends Car{
 
     // TODO fix this method according to lab pm
     public void gas(double amount){
-        incrementSpeed(amount);
+        if (!(amount < 0 || amount > 1)) {
+            incrementSpeed(amount);
+        }
     }
 
     // TODO fix this method according to lab pm
     public void brake(double amount){
-        decrementSpeed(amount);
+        if (!(amount < 0 || amount > 1)) {
+            decrementSpeed(amount);
+        }
     }
 
 
