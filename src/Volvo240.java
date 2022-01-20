398ce73e9ed98b63e7edda3a972afedcc35168ec
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-01-20 17:47:19 +0100

    Gas && break volvo240

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -22,13 +22,18 @@ public class Volvo240 extends Car{
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
 
-    // TODO fix this method according to lab pm
     public void gas(double amount){
+        if(!(amount < 0 || amount > 1))
         incrementSpeed(amount);
+
+
+
+
     }
 
     // TODO fix this method according to lab pm
     public void brake(double amount){
+        if(!(amount < 0 || amount > 1))
         decrementSpeed(amount);
     }
 
