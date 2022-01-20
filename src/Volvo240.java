f400ec9bbac2d112c411d23911b43e7a843a8393
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-01-20 17:59:07 +0100

    Update Volvo240.java

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -1,50 +1,49 @@
 import java.awt.*;
 
-public class Volvo240 extends Car{
+public class Volvo240 extends Car {
 
     public final static double trimFactor = 1.25;
 
 
-    public Volvo240(){
+    public Volvo240() {
         super(4, 100, Color.black, "Volvo240", 0, 0);
         stopEngine();
     }
 
-    private double speedFactor(){
+    private double speedFactor() {
         return enginePower * 0.01 * trimFactor;
     }
 
-    private void incrementSpeed(double amount){
+    private void incrementSpeed(double amount) {
         double preCurrentSpeed = getCurrentSpeed();
-        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-        if (currentSpeed > enginePower){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
+        if (currentSpeed > enginePower) {
             currentSpeed = preCurrentSpeed;
         }
     }
 
-    private void decrementSpeed(double amount){
+    private void decrementSpeed(double amount) {
         double preCurrentSpeed = getCurrentSpeed();
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
-        if (currentSpeed < 0){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
+        if (currentSpeed < 0) {
             currentSpeed = preCurrentSpeed;
         }
     }
 
-    public void gas(double amount){
-        if(!(amount < 0 || amount > 1))
-        incrementSpeed(amount);
-
-
+    public void gas(double amount) {
+        if (!(amount < 0 || amount > 1)) {
+            incrementSpeed(amount);
+        }
 
 
     }
 
     // TODO fix this method according to lab pm
-    public void brake(double amount){
-        if(!(amount < 0 || amount > 1))
-        decrementSpeed(amount);
+    public void brake(double amount) {
+        if (!(amount < 0 || amount > 1)) {
+            decrementSpeed(amount);
+        }
     }
-
 
 
 }
