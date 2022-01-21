58108ff39310dd3c6467cc96ce4da7e73ab5b8ad
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-01-21 15:43:33 +0100

    Update Saab95.java

--- src/Saab95.java
+++ src/Saab95.java
@@ -36,11 +36,23 @@ public class Saab95 extends Car{
         turboOn = false;
     }
 
+    /**
+     * speedFactor(); is a representation of a factor that can be utilized in order to either increase or decrease
+     * the speed.
+     * @return the factor of what the speed is either going to be increased/decreased with.
+     */
+
     private double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
+
+    /**
+     * incrementSpeed() sets the currentSpeed to a speed of a higher value in comparison
+     * with the speed in the previous state.
+     * @param amount is the amount of speed that the current speed is going to be increased by.
+     */
 
     private void incrementSpeed(double amount){
         double preCurrentSpeed = getCurrentSpeed();
@@ -49,8 +61,13 @@ public class Saab95 extends Car{
             currentSpeed = preCurrentSpeed;
         }
     }
+    /**
+     * decrementSpeed() sets the currentSpeed to a speed of a lower value in comparison
+     * with the speed in the previous state.
+     * @param amount is the amount of speed that the current speed is going to be decreased by.
+     */
 
-    
+
     private void decrementSpeed(double amount){
         double preCurrentSpeed = getCurrentSpeed();
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
@@ -59,11 +76,24 @@ public class Saab95 extends Car{
         }
     }
 
+    /**
+     * The method gas(); is a representation that increases the speed for the car by calling incrementSpeed();.
+     * @param amount is the amount of speed that the car is going to be increased by within
+     * a certain interval.
+     */
+
+
     public void gas(double amount){
         if (!(amount < 0 || amount > 1)) {
             incrementSpeed(amount);
         }
     }
+
+    /**
+     * The method brake(); is a representation that decreases the speed for the car by calling decrementSpeed();.
+     * @param amount is the amount of speed that the car is going to be decreased by within
+     * a certain interval.
+     */
 
 
     public void brake(double amount){
