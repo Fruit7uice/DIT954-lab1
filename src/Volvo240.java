87dc2ee50575da727e985b7fe1768a47a280841d
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-01-21 15:40:03 +0100

    Javadoc Volvo240 methods

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -2,21 +2,29 @@ import java.awt.*;
 
 public class Volvo240 extends Car {
 
-    public final static double trimFactor = 1.25;
+    public final static double trimFactor = 1.25; // A decimal which determine the speed factor of the car.
 
     /**
-     * Constructor for the Volvo240 class. The class calls for the superclass, car, which enables Volvo240 to access the
-     * data and methods declared in Car.
+     * Constructor for the Volvo240 class. The class calls for the superclass, Car, which enables Volvo240 to inherit
+     * variables declared in Car.
      */
     public Volvo240() {
         super(4, 100, Color.black, "Volvo240", 0, 0);
         stopEngine();
     }
 
+    /**
+     * Speed factor of a car is a representation of a factor which can be used to increase the speed of the car.
+     * @return the calculated factor of which the speed increases by.
+     */
     private double speedFactor() {
         return enginePower * 0.01 * trimFactor;
     }
 
+    /**
+     * Sets the speed(increasing) of the car controlled by the amount inputted.
+     * @param amount is the restricted amount which can be multiplied to increase speed.
+     */
     private void incrementSpeed(double amount) {
         double preCurrentSpeed = getCurrentSpeed();
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
@@ -25,6 +33,10 @@ public class Volvo240 extends Car {
         }
     }
 
+    /**
+     * Sets the speed(decreasing) of the car controlled by the amount inputted.
+     * @param amount is the restricted amount which can be multiplied to decrease speed.
+     */
     private void decrementSpeed(double amount) {
         double preCurrentSpeed = getCurrentSpeed();
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
@@ -33,14 +45,22 @@ public class Volvo240 extends Car {
         }
     }
 
-
+    /**
+     * A representation of pressing the gas.
+     * Calls incrementSpeed() when amount is within a certain interval.
+     * @param amount is the amount pressed on the gas represented by a decimal number.
+     */
     public void gas(double amount) {
         if (!(amount < 0 || amount > 1)) {
             incrementSpeed(amount);
         }
     }
 
-
+    /**
+     * A representation of pressing the brake.
+     * Calls decrementSpeed() when amount is within a certain interval.
+     * @param amount is the amount pressed on the brake represented by a decimal number.
+     */
     public void brake(double amount) {
         if (!(amount < 0 || amount > 1)) {
             decrementSpeed(amount);
