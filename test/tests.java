5d02cc28a3cfb6c1b93190d494e4cb2f27276bb2
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-01-21 16:23:21 +0100

    testEnginePower();

--- test/tests.java
+++ test/tests.java
@@ -6,6 +6,20 @@ import static org.junit.Assert.*;
 public class tests {
 
 
+    @Test
+    public void testEnginePower(){
+        Saab95 saab95 = new Saab95();
+        Volvo240 volvo240 = new Volvo240();
+        assertTrue(volvo240.enginePower < saab95.enginePower);
+    }
+
+
+    
+
+
+
+
+
 
 
 
