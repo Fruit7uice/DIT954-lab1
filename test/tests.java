ee521e8ba2554b724bf65dbad9a9e4a9da63dc5a
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-01-21 16:16:36 +0100

    getNDoors Tests JUnit

--- test/tests.java
+++ test/tests.java
@@ -6,8 +6,11 @@ import static org.junit.Assert.*;
 public class tests {
 
 
-
-
+    @Test
+    public void getNrDoorsTest(){
+        Volvo240 car = new Volvo240();
+        assertTrue(car.getNrDoors() == 4);
+    }
 
 
 
