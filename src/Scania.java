2e94e8447e6a43d0978faa93bbbd51d91651492a
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-01-31 13:39:23 +0100

    Initial Scania

---
+++ src/Scania.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Scania extends Car {
+
+    /**
+     * The constructor of the car class.
+     */
+    public Scania() {
+        super(2, 770, Color.WHITE, "Scania", 0, 0);
+    }
+
+    
+
+
+}
