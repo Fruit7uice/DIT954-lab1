a388e5df8a6e0ccb2e04f1beab2a088642657617
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-01-31 13:47:45 +0100

    Scania truckbed

--- src/Scania.java
+++ src/Scania.java
@@ -2,14 +2,17 @@ import java.awt.*;
 
 public class Scania extends Car {
 
+    private double truckBedAngle;
+
     /**
      * The constructor of the car class.
      */
     public Scania() {
         super(2, 770, Color.WHITE, "Scania", 0, 0);
+        truckBedAngle = 0;
     }
 
-    
+
 
 
 }
