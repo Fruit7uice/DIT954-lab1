96a5f8f2010134fc6d219af70cda942276b42fb0
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-02-03 15:02:21 +0100

    Update CarWorkShop();

--- src/CarWorkshop.java
+++ src/CarWorkshop.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarWorkshop <T extends Car>{
+    private double maxCapacity;
+    private double nCarsInWorkshop;
 
-    private List<T> carWorkshop = new ArrayList<>();
+    public CarWorkshop(List<T> carWorkshop, int maxCapacity, int nCarsInWorkshop) {
+        this.carWorkshop = carWorkshop;
+        this.maxCapacity = maxCapacity;
+        this.nCarsInWorkshop = nCarsInWorkshop;
+    }
+
+    private List<T> carWorkshop = new ArrayList<>(5);
 
     T retrieveCar() {
         return carWorkshop.remove(0);
@@ -13,4 +21,27 @@ public class CarWorkshop <T extends Car>{
         carWorkshop.add(car);
     }
 
+    public double getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(double maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public double getNCarsInWorkshop() {
+        return nCarsInWorkshop;
+    }
+
+    public void setNCarsInWorkshop(double nCarsInWorkshop) {
+        this.nCarsInWorkshop = nCarsInWorkshop;
+    }
+
+    public static void main(String[] args) {
+        CarWorkshop<Saab95> saab95Workshop = new ArrayList<>();
+        Volvo240 volvo240 = new Volvo240();
+        Saab95 saab95 = new Saab95();
+        saab95Workshop.handInCar(volvo240);
+        saab95Workshop.handInCar(saab95);
+    }
 }
