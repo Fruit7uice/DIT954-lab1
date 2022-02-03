7fc1004e31d0f2093103fe68099d75441ee033c1
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-02-03 15:43:08 +0100

    Update CarWorkshop.java

--- src/CarWorkshop.java
+++ src/CarWorkshop.java
@@ -25,25 +25,26 @@ public class CarWorkshop<T extends Car> {
     public double getMaxCapacity() {
         return maxCapacity;
     }
-
     public void setMaxCapacity(double maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
-
     public double getNCarsInWorkshop() {
         return nCarsInWorkshop;
     }
-
     public void setNCarsInWorkshop(double nCarsInWorkshop) {
         this.nCarsInWorkshop = nCarsInWorkshop;
     }
 
     public static void main(String[] args) {
-        CarWorkshop<Saab95> saab95Workshop = new CarWorkshop<>(5,0);
+        CarWorkshop<Saab95> saab95Workshop = new CarWorkshop<>(5);
         Volvo240 volvo240 = new Volvo240();
         Saab95 saab95 = new Saab95();
         saab95Workshop.handInCar(volvo240);
         saab95Workshop.handInCar(saab95);
         saab95Workshop.retrieveCar(volvo240);
+        CarWorkshop<Car> workshop = new CarWorkshop<>(5);
+        workshop.handInCar(volvo240);
+
+
     }
 }
