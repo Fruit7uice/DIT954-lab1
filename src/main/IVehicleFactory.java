73c7ec436e7db744d66d679004349b4744a48d64
JOELDENFETE <90786954+JOELDENFETE@users.noreply.github.com>
2022-02-24 14:17:45 +0100

    VehicleFactory

--- src/main/IVehicleFactory.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-public interface IVehicleFactory {
-
-    Vehicle createVehicle(Vehicle newVehicleType);
-}
--- src/main/RandomVehicleFactory.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class RandomVehicleFactory implements IVehicleFactory {
-
-    @Override
-    public Vehicle createVehicle(Vehicle newVehicleType) {
-        return null;
-    }
-}
