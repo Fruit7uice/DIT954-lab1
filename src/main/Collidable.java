d1c6b19cf616890921bddf6ab5dd1dddcd32f7f1
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-02-25 16:01:38 +0100

    try

--- src/main/Collidable.java
+++ src/main/Collidable.java
@@ -8,13 +8,26 @@ public abstract class Collidable extends Positionable {
         super(xCord, yCord, width, height, length);
     }
 
-    public boolean isCollisionWithOther(Positionable other) {
-        boolean above = other.getMaxY() < this.getY();
-        boolean below = other.getY() > this.getMaxY();
-        boolean leftOf = other.getMaxX() < this.getX();
-        boolean rightOf = other.getX() > this.getMaxX();
-        setLatestCollisionDirection(above, below, leftOf, rightOf);
-        return !(above || below || leftOf || rightOf);
+
+
+    public void vehicleCollision(Vehicle vehicle, Collidable other) {
+        //check X movement bounce
+        if (getMaxX() + vehicle.getdX() > other.getX() && getX() + vehicle.getdX() < other.getMaxX() &&
+                getMaxY() > other.getY() && getY() < other.getMaxY()) {
+            vehicle.stopEngine();
+            vehicle.setdX(vehicle.getdX()* -1);
+            vehicle.startEngine();
+            vehicle.move();
+        }
+
+        //check Y movement bounce
+        if (getMaxX()> other.getX() && getX() < other.getMaxX() &&
+                getMaxY() + vehicle.getdY() > other.getY() && getY() + vehicle.getdY() < other.getMaxY()) {
+            vehicle.stopEngine();
+            vehicle.setdY(vehicle.getdY()*-1);
+            vehicle.startEngine();
+            vehicle.move();
+        }
 
     }
 
